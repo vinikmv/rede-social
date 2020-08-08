2ba6cc6675cbc9b888e4850889e4a2d09ce47c54
Vinicius Vicente <viniciuskmv@gmail.com>
2020-08-08 16:42:19 -0300

    Adicionado rotas de deletar post e  notificações para likes e comentarios

--- functions/handlers/posts.js
+++ functions/handlers/posts.js
@@ -19,20 +19,209 @@ exports.getAllPosts = (req, res) => {
     .catch((err) => console.error(err));
 };
 
-exports.criarUmNovoPost = (req, res) => {
+exports.createNewPost = (req, res) => {
   const newPost = {
     body: req.body.body,
     userHandle: req.user.handle,
+    userImage: req.user.imageUrl,
     createdAt: new Date().toISOString(),
+    likeCount: 0,
+    commentPost: 0,
   };
 
   db.collection("posts")
     .add(newPost)
     .then((doc) => {
-      res.json({ message: `document ${doc.id} created successfully` });
+      const resPost = newPost;
+      resPost.postId = doc.id;
+      res.json(resPost);
     })
     .catch((err) => {
       res.status(500).json({ error: "Something went wrong" });
       console.log(err);
+    });
+};
+
+//Retorna um post com o comentário
+
+exports.getPost = (req, res) => {
+  let postData = {};
+
+  db.doc(`/posts/${req.params.postId}`)
+    .get()
+    .then((doc) => {
+      if (!doc.exists) {
+        return res.status(404).json({ error: "Post não encontrado" });
+      }
+      postData = doc.data();
+      postData.postId = doc.id;
+      return db
+        .collection("comments")
+        .orderBy("createdAt", "desc")
+        .where("postId", "==", req.params.postId)
+        .get();
+    })
+    .then((data) => {
+      postData.comments = [];
+      data.forEach((doc) => {
+        postData.comments.push(doc.data());
+      });
+      return res.json(postData);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({ error: err.code });
+    });
+};
+
+//Realizar um comentário
+
+exports.commentOnPost = (req, res) => {
+  if (req.body.body.trim() === "")
+    return res.status(400).json({ Erro: "Campo não pode estar vazio" });
+
+  const newComment = {
+    body: req.body.body,
+    createdAt: new Date().toISOString(),
+    postId: req.params.postId,
+    userHandle: req.user.handle,
+    userImage: req.user.imageUrl,
+  };
+
+  db.doc(`/posts/${req.params.postId}`)
+    .get()
+    .then((doc) => {
+      if (!doc.exists) {
+        return res.status(404).json({ Erro: "Post não encontrado" });
+      }
+      return doc.ref.update({ commentCount: doc.data().commentCount + 1 });
+    })
+    .then(() => {
+      return db.collection("comments").add(newComment);
+    })
+    .then(() => {
+      res.json(newComment);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ Erro: "Algo deu errado" });
+    });
+};
+
+// Curtir um post
+
+exports.likePost = (req, res) => {
+  const likeDocument = db
+    .collection("likes")
+    .where("userHandle", "==", req.user.handle)
+    .where("postId", "==", req.params.postId)
+    .limit(1);
+
+  const postDocument = db.doc(`/posts/${req.params.postId}`);
+
+  let postData;
+
+  postDocument
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        postData = doc.data();
+        postData.postId = doc.id;
+        return likeDocument.get();
+      } else {
+        return res.status(404).json({ error: "Post não encontrado" });
+      }
+    })
+    .then((data) => {
+      if (data.empty) {
+        return db
+          .collection("likes")
+          .add({
+            postId: req.params.postId,
+            userHandle: req.user.handle,
+          })
+          .then(() => {
+            postData.likeCount++;
+            return postDocument.update({ likeCount: postData.likeCount });
+          })
+          .then(() => {
+            return res.json(postData);
+          });
+      } else {
+        return res.status(400).json({ error: "Post já curtido" });
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({ error: err.code });
+    });
+};
+
+exports.unlikePost = (req, res) => {
+  const likeDocument = db
+    .collection("likes")
+    .where("userHandle", "==", req.user.handle)
+    .where("postId", "==", req.params.postId)
+    .limit(1);
+
+  const postDocument = db.doc(`/posts/${req.params.postId}`);
+
+  let postData;
+
+  postDocument
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        postData = doc.data();
+        postData.postId = doc.id;
+        return likeDocument.get();
+      } else {
+        return res.status(404).json({ error: "Post não encontrado" });
+      }
+    })
+    .then((data) => {
+      if (data.empty) {
+        return res.status(400).json({ error: "Post ainda não curtido" });
+      } else {
+        return db
+          .doc(`/likes/${data.docs[0].id}`)
+          .delete()
+          .then(() => {
+            postData.likeCount--;
+            return postDocument.update({ likeCount: postData.likeCount });
+          })
+          .then(() => {
+            res.json(postData);
+          });
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({ error: err.code });
+    });
+};
+
+//Deletar um post
+
+exports.deletePost = (req, res) => {
+  const document = db.doc(`/posts/${req.params.postId}`);
+  document
+    .get()
+    .then((doc) => {
+      if (!doc.exists) {
+        return res.status(404).json({ error: "Post não encontrado" });
+      }
+      if (doc.data().userHandle !== req.user.handle) {
+        return res.status(403).json({ error: "Não autorizado" });
+      } else {
+        return document.delete();
+      }
+    })
+    .then(() => {
+      res.json({ message: "Post apagado com sucesso" });
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.status(400).json({ error: err.code });
     });
 };
--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,14 @@
 const functions = require("firebase-functions");
 
-const { getAllPosts, criarUmNovoPost } = require("./handlers/posts");
+const {
+  getAllPosts,
+  createNewPost,
+  getPost,
+  commentOnPost,
+  deletePost,
+  likePost,
+  unlikePost,
+} = require("./handlers/posts");
 const {
   signUp,
   login,
@@ -12,11 +20,17 @@ const {
 const fbAuth = require("./util/fbAuth");
 
 const express = require("express");
+const { db } = require("./util/admin");
 const app = express();
 
 // Posts Route
 app.get("/posts", getAllPosts);
-app.post("/posts", fbAuth, criarUmNovoPost);
+app.get("/post/:postId", getPost);
+app.get("/post/:postId/like", fbAuth, likePost);
+app.get("/post/:postId/unlike", fbAuth, unlikePost);
+app.post("/post", fbAuth, createNewPost);
+app.post("/post/:postId/comment", fbAuth, commentOnPost);
+app.delete("/post/:postId", fbAuth, deletePost);
 
 //Users Route
 app.post("/signup", signUp);
@@ -26,3 +40,69 @@ app.post("/user", fbAuth, addUserDetails);
 app.get("/user", fbAuth, getAuthenticatedUser);
 
 exports.api = functions.https.onRequest(app);
+
+exports.createNotificationOnLike = functions.firestore
+  .document("likes/{id")
+  .onCreate((snapshot) => {
+    db.doc(`/posts/${snapshot.data().postId}`)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          return db.doc(`/notifications/${snapshot.id}`).set({
+            createdAt: new Date().toISOString(),
+            recipient: doc.data().userHandle,
+            sender: snapshot.data().userHandle,
+            type: "like",
+            read: false,
+            postId: doc.id,
+          });
+        }
+      })
+      .then(() => {
+        return;
+      })
+      .catch((err) => {
+        console.error(err);
+        return;
+      });
+  });
+
+exports.deleteNotificationOnUnlike = functions.firestore
+  .document("comments/{id")
+  .onDelete((snapshot) => {
+    db.doc(`/notifications/${snapshot.id}`)
+      .delete()
+      .then(() => {
+        return;
+      })
+      .catch((err) => {
+        console.error(err);
+        return;
+      });
+  });
+
+exports.createNotificationOnComment = functions.firestore
+  .document("comments/{id")
+  .onCreate((snapshot) => {
+    db.doc(`/posts/${snapshot.data().postId}`)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          return db.doc(`/notifications/${snapshot.id}`).set({
+            createdAt: new Date().toISOString(),
+            recipient: doc.data().userHandle,
+            sender: snapshot.data().userHandle,
+            type: "comment",
+            read: false,
+            postId: doc.id,
+          });
+        }
+      })
+      .then(() => {
+        return;
+      })
+      .catch((err) => {
+        console.error(err);
+        return;
+      });
+  });
